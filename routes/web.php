be21dedd567f20e7d358d413793ddf037b86d025
Muxsin <muxsin.shokirov@gmail.com>
2024-07-31 00:14:20 +0500

    Created method for listing products

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => 'v1'], function() {
+    Route::get('/products', [ProductController::class, 'index']);
 });
