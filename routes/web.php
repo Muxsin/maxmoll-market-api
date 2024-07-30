be30b7cdab20fd1f253d78f7a739d33b34590897
Muxsin <muxsin.shokirov@gmail.com>
2024-07-31 00:22:45 +0500

    Configured api routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,1 @@
 <?php
-
-use App\Http\Controllers\ProductController;
-use Illuminate\Support\Facades\Route;
-
-Route::group(['prefix' => 'v1'], function() {
-    Route::get('/products', [ProductController::class, 'index']);
-});
