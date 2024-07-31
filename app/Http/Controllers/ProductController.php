d7766958c700f9203ff0c495f6a78b833c6f0100
Muxsin <muxsin.shokirov@gmail.com>
2024-07-31 05:38:15 +0500

    Listing products with their stocks

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ProductResource;
 use App\Models\Product;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        return Product::all();
+        $products = Product::with('stocks.warehouse')->get();
+
+        return ProductResource::collection($products);
     }
 }
--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Stock extends Model
 {
     use HasFactory;
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function warehouse()
+    {
+        return $this->belongsTo(Warehouse::class);
+    }
 }
