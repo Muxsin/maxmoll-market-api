06878f79e8e979e0f1eaadd90a3a508a0c556c88
Muxsin <muxsin.shokirov@gmail.com>
2024-08-06 18:01:46 +0500

    Added comments for ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,8 +9,10 @@ class ProductController extends Controller
 {
     public function index()
     {
+        // Получаем все продукты вместе с их запасами и связями со складами
         $products = Product::with('stocks.warehouse')->get();
 
+        // Возвращаем коллекцию продуктов, преобразованную с помощью ресурса ProductResource
         return ProductResource::collection($products);
     }
 }
